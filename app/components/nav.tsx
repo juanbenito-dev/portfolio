7ba235065be4f931a2f888ddb4b8a3babcb6403d
juanbenito-dev <132196530+juanbenito-dev@users.noreply.github.com>
2026-04-12 11:30:13 +0200

    feat: add "JB" logo to nav & remove unnecessary responsive logic & classes

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,45 +1,37 @@
-"use client";
-
+import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
-import Hamburger from "@/app/components/icons/hamburger";
 
 const links = [
-  { name: "Projects", href: "/projects" },
   { name: "About", href: "/about" },
+  { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
 ];
 
 export default function Nav() {
-  const [isMobileNavExpanded, setIsMobileNavExpanded] = useState(false);
-
-  function handleClick() {
-    setIsMobileNavExpanded(!isMobileNavExpanded);
-  }
-
   return (
-    <header
-      className={`bg-secondary sticky top-0 z-10 flex flex-wrap items-center justify-between p-2 ${isMobileNavExpanded ? "" : "border-be-neutral/50 border-be"}`}
-    >
-      <Link href="/" className="text-xl">
-        Juan Benito
-      </Link>
+    <header className="bg-secondary/50 sticky top-5 z-10 mx-auto my-5 w-7/8 overflow-hidden rounded-xl p-4">
+      <div className="absolute inset-0 h-[200%] backdrop-blur-lg"></div>
 
-      <button type="button" onClick={handleClick} className="md:hidden">
-        <Hamburger />
-      </button>
+      <div className="relative flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/jb.svg"
+            alt="'JB' logo (short for Juan Benito)"
+            width={30}
+            height={30}
+          />
+        </Link>
 
-      <nav
-        className={`border-bs-neutral/50 absolute inset-x-0 top-full border-bs bg-inherit p-2 md:static md:block md:border-none ${isMobileNavExpanded ? "block" : "hidden"}`}
-      >
-        <ul className="flex flex-col items-center md:flex-row">
-          {links.map((link) => (
-            <Link key={link.name} href={link.href}>
-              {link.name}
-            </Link>
-          ))}
-        </ul>
-      </nav>
+        <nav>
+          <ul className="flex items-center gap-x-5">
+            {links.map((link) => (
+              <Link key={link.name} href={link.href}>
+                {link.name}
+              </Link>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
